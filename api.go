da4b19e2787b4b02a005fdf26e45ddc8d44f858f
Giacomo Marinangeli <giacomo@cloudacademy.com>
2015-09-06 21:00:01 +0200

    Fetch location and current weather from weather.com and print the probability of precitipation

---
+++ api.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	baseUrl     string = "http://api.wunderground.com/api/%s%%s"
+	geoLookupEP string = "/geolookup/q/autoip.json"
+	hourlyEP    string = "/hourly%s.json"
+)
+
+type weatherApi struct {
+	token string
+}
+
+func newWeatherApi(token string) *weatherApi {
+	return &weatherApi{token}
+}
+
+func (wa *weatherApi) getGeoLookup() (*geoLookup, error) {
+	res, err := wa.getEndpoint(geoLookupEP)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	gl := &geoLookup{}
+
+	enc := json.NewDecoder(res.Body)
+	err = enc.Decode(gl)
+	if err != nil {
+		return nil, err
+	}
+	if gl.Location.L == "" {
+		return nil, fmt.Errorf("unknown location")
+	}
+	return gl, nil
+}
+
+func (wa *weatherApi) getHourly(gl *geoLookup) (*hourlyData, error) {
+
+	res, err := wa.getEndpoint(hourlyEP, gl.Location.L)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	hd := &hourlyData{}
+	err = hd.decodeFromJson(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return hd, nil
+}
+
+func (wa *weatherApi) getEndpoint(endpoint string, params ...interface{}) (*http.Response, error) {
+	url := fmt.Sprintf(baseUrl, wa.token)
+	if len(params) != 0 {
+		endpoint = fmt.Sprintf(endpoint, params...)
+	}
+	url = fmt.Sprintf(url, endpoint)
+	r, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+type geoLookup struct {
+	Location struct {
+		Country string `json:"country_iso3166"`
+		City    string
+		L       string
+	}
+}
+
+type FCTTime struct {
+	Epoch int64 `json:"epoch,string"`
+}
+
+func (e *FCTTime) getTime() time.Time {
+	return time.Unix(e.Epoch, 0)
+}
+
+type hourlyData struct {
+	HourlyForecast []struct {
+		FCTTIME FCTTime `json="FCTTIME"`
+		Pop     int32   `json:",string"`
+	} `json:"hourly_forecast"`
+}
+
+func (hd *hourlyData) decodeFromJson(r io.Reader) (err error) {
+	enc := json.NewDecoder(r)
+	err = enc.Decode(hd)
+	return
+}
+
+func readFromFile(filename string) (*hourlyData, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	hd := &hourlyData{}
+	err = hd.decodeFromJson(f)
+	if err != nil {
+		return nil, err
+	}
+	return hd, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var token string
+
+func init() {
+	flag.StringVar(&token, "token", "", "Token for Weather.com API")
+	flag.Parse()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
+	wa := newWeatherApi(token)
+
+	gl, err := wa.getGeoLookup()
+	checkError(err)
+
+	hd, err := wa.getHourly(gl)
+	checkError(err)
+
+	fmt.Printf("%v (%v)\n", gl.Location.City, gl.Location.Country)
+	for _, h := range hd.HourlyForecast {
+		fmt.Printf("%2d: ", h.FCTTIME.getTime().Hour())
+		for i := int32(0); i < (h.Pop/10)+1; i++ {
+			fmt.Print("|")
+		}
+		fmt.Printf(" %d%%\n", h.Pop)
+	}
 
 }
